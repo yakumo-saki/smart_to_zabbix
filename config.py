c53955ec68f36abb44b9b20d81522583786e43b9
Yakumo Saki <yakumo@ziomatrix.org>
2021-09-11 02:14:56 +0900

    tweak smartctl option

--- config.py
+++ config.py
@@ -15,9 +15,9 @@ LOG_LEVEL = os.environ.get('LOG_LEVEL', "DEBUG")
 # 以下はZabbixテンプレートと整合性を取る必要がある
 
 #  
-LINUX_SMARTCTL_SCAN_CMD = ['sudo', 'smartctl', '--json', '--scan', '-d', 'sat', '-d', 'nvme', '-d', 'ata', '-d', 'scsi']
-LINUX_SMARTCTL_DETAIL_CMD = ['sudo', 'smartctl', '--json', '-a', '-d', 'sat', '-d', 'nvme', '-d', 'ata', '-d', 'scsi']
+LINUX_SMARTCTL_SCAN_CMD = ['sudo', 'smartctl', '--json', '--scan', '-d', 'sat', '-d', 'nvme', '-d', 'ata']
+LINUX_SMARTCTL_DETAIL_CMD = ['sudo', 'smartctl', '--json', '-a', '-d', 'sat', '-d', 'nvme', '-d', 'ata']
 
 # 
-WIN_SMARTCTL_SCAN_CMD = ['smartctl', '--json', '--scan', '-d', 'sat']
-WIN_SMARTCTL_DETAIL_CMD = ['smartctl', '--json', '-a', '-d', 'sat']
+WIN_SMARTCTL_SCAN_CMD = ['smartctl', '--json', '--scan', '-d', 'sat', '-d', 'nvme', '-d', 'ata']
+WIN_SMARTCTL_DETAIL_CMD = ['smartctl', '--json', '-a', '-d', 'sat', '-d', 'nvme', '-d', 'ata']
