abdd92642f8ab9c89acbe36a61d895feb61d34b9
Yakumo Saki <yakumo@ziomatrix.org>
2021-09-11 23:51:20 +0900

    add intel interpriter

---
+++ interpriters/smart/IntelX25Interpriter.py
@@ -0,0 +1,36 @@
+import logging
+
+from interpriters.smart.SmartBaseInterpriter import SmartBaseInterpriter
+from modules.const import Keys, Unit
+from interpriters.tbw_estimate import tbw_estimate
+
+logger = logging.getLogger(__name__)
+
+"""
+SanDisk SSDSSDH3 -> Marvel 88SS1074
+"""
+class IntelX25Interpriter(SmartBaseInterpriter):
+    # def __init__(self):
+    #     pass
+
+    def isTargetStrict(self, data):
+        if (data["model_family"].startswith("Intel X18-M/X25-M/X25-V G2 SSDs")):
+            return True
+
+        return False
+
+    def isTargetLoose(self, data):
+        return (data["model_name"].startswith("INTEL"))
+
+    def parse(self, data):
+        logger.debug("IntelX25")
+        ret = self.basic_parse(data)
+
+        # 233 Media Wearout Indicator
+        ret[Keys.SSD_LIFESPAN] = self.get_smart_value(data, 233)
+
+        # 241 Total_LBAs_Written は名前に反してそのままGB単位
+        hostWrites = self.get_smart_raw_value(data, 225)
+        ret[Keys.SSD_BYTES_WRITTEN] = (hostWrites * 32 * Unit.MB)
+
+        return ret
