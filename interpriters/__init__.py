426f70ea3d99093b110c9bbbb01408f63f002126
Yakumo Saki <yakumo@ziomatrix.org>
2021-09-29 20:42:19 +0900

    add intel x25 interpriter

--- interpriters/__init__.py
+++ interpriters/__init__.py
@@ -1,7 +1,8 @@
+from interpriters.smart.IntelX25Interpriter import IntelX25Interpriter
 from interpriters.smart.SmartBasicInterpriter import SmartBasicInterpriter
 from interpriters.smart.SanDiskInterpriter import SmartSanDiskInterpriter
 from interpriters.nvme.NvmeBasicInterpriter import NvmeBasicInterpriter
 
 
-SPECIAL_INTERPRITERS = [SmartSanDiskInterpriter()]
+SPECIAL_INTERPRITERS = [SmartSanDiskInterpriter(), IntelX25Interpriter()]
 BASIC = [SmartBasicInterpriter(), NvmeBasicInterpriter()]
